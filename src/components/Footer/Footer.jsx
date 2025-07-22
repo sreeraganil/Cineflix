32abaf859ba3b8b8a6230f3b9dd1ef74dda68d89
sreeraganil <sreeraganil2003@gmail.com>
2025-07-22 15:26:57 +0530

    webmaster

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-content">
-          <p>&copy; <span id="current-year"></span> Cineflix. All rights reserved.</p>
+          <p>&copy; <span id="current-year"></span> Cineo. All rights reserved.</p>
           <nav>
               <ul>
                   <li><a href="/privacy-policy">Privacy Policy</a></li>
@@ -13,11 +13,11 @@ const Footer = () => {
                   <li><a href="/contact">Contact Us</a></li>
               </ul>
           </nav>
-          <address>
+          {/* <address>
               <p>123 Your Street, Your City, Your State, 12345</p>
               <p>Email: <a style={{color:"red"}} href="mailto:info@cineflix.com">info@cineflix.com</a></p>
               <p>Phone: (+91) 9876543210</p>
-          </address>
+          </address> */}
       </div>
     </footer>
   );
